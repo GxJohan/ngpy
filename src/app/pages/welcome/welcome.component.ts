6c70b58889d6622e04b79269aa8826dfc636af12
Johan López <johamax@gmail.com>
2024-10-28 15:12:35 -0500

    v1.1

---
+++ src/app/pages/welcome/welcome.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css'],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzInputModule,
+    NzCheckboxModule
+  ],
+})
+export class WelcomeComponent implements OnInit {
+  validateForm: FormGroup;
+
+  constructor(private fb: NonNullableFormBuilder) {
+    this.validateForm = this.fb.group({
+      userName: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      remember: [true]
+    });
+  }
+
+  ngOnInit(): void {
+    // Implementation of ngOnInit
+  }
+
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log('submit', this.validateForm.value);
+    } else {
+      Object.values(this.validateForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+}
